8980f7bf59ab188b37a9991f5553a6fb0ddba4c3
Bewi <benjamin.wilfart@gmail.com>
2024-11-15 22:05:11 +0100

    Use translated labels

---
+++ astro/src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  en: "English",
+  fr: "Français"
+}
+
+export const defaultLang = "fr";
+
+export const ui = {
+  en: {
+    "content.title": "List of bands",
+    "band.card.title": "Band card title",
+    "band.page.title": "Band:",
+    "band.page.genraLabel": "Genra",
+    "band.page.fanLabel": "Discovered by"
+  },
+  fr: {
+    "content.title": "Liste de groupes",
+    "band.card.title": "Groupe",
+    "band.page.title": "Groupe:",
+    "band.page.genraLabel": "Genre",
+    "band.page.fanLabel": "Decouvert part"
+  }
+}
