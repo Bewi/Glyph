0bede71fd7f240486169c0595f360c5728ecfdbc
Bewi <benjamin.wilfart@gmail.com>
2024-11-20 20:58:02 +0100

    wip: deploy.yaml

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bewi.github.io',
-  base: 'Glyph',
+  // base: 'Glyph',
 });
