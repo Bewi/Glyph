4a28d1c78a0305f1767def8a4dce80cece8c5223
Bewi <benjamin.wilfart@gmail.com>
2024-11-26 20:28:09 +0100

    Hero component

--- astro/src/i18n/ui.ts
+++ astro/src/i18n/ui.ts
@@ -7,7 +7,9 @@ export const defaultLang = "fr";
 
 export const ui = {
   en: {
-    "index.title": "Welcome to Glyph!",
+    "index.title": "Glyph",
+    "hero.text-1": "EN Translation to come",
+    "hero.text-2": "EN Translation to come",
     "content.title": "List of bands",
     "band.card.title": "Band card title",
     "band.page.title": "Band:",
@@ -15,7 +17,9 @@ export const ui = {
     "band.page.fanLabel": "Discovered by"
   },
   fr: {
-    "index.title": "Bienvenue chez Glyph!",
+    "index.title": "Glyph!",
+    "hero.text-1": "Nous créons des services, des produits digitaux et des outils de travail en prenant en compte vos exigences et vos utilisateurs",
+    "hero.text-2": "Du besoin le plus simple au plus complexe, nous pouvons vous aider !",
     "content.title": "Liste de groupes",
     "band.card.title": "Groupe",
     "band.page.title": "Groupe:",
