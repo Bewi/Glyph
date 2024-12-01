91654626c916ab2c4786ecedfc3f29e00980dc56
Bewi <benjamin.wilfart@gmail.com>
2024-12-01 18:46:12 +0100

    Fix site url

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bewi.github.io',
+  site: 'https://maisallerquoi.netlify.app',
   base: '/',
 });
